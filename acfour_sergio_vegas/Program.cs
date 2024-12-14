d1c37b1dc0ea1a4a0363eb5f6a0f96d63011590f
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2024-12-14 20:09:37 +0100

    Add project files.

---
+++ acfour_sergio_vegas/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace acfour_sergio_vegas
+{
+    public static class implementacioDiagrama
+    {
+        //Constants
+        const string MsgSizeArray = "Introdueix el tamany que vols enmagatzemar.";
+        const string MsgUserNumb = "Introdueix un numero.";
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(MsgSizeArray);
+            int sizeArray = int.Parse(Console.ReadLine());
+
+            int[] arrayNumb = new int[sizeArray];
+
+            for (int i = 0; i < sizeArray; i++)
+            {
+                Console.WriteLine(MsgUserNumb);
+                int userNumb = int.Parse(Console.ReadLine());
+                arrayNumb[i] = userNumb;
+            }
+            WriteArray(arrayNumb);
+        }
+        public static void WriteArray(int[] arrayNumb)
+        {
+            for (int i = 0; i < arrayNumb.Length; i++)
+            {
+                Console.Write(arrayNumb[i] + " ");
+            }
+        }
+        public static int GestioError()
+        {
+            int userNumb = 0;
+            try { userNumb = int.Parse(Console.ReadLine()); }
+            catch (FormatException) { Console.WriteLine("Dades introduides incorrectes"); }
+
+            return userNumb;
+        }
+    }
+}
