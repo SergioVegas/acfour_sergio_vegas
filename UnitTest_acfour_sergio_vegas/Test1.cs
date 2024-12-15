470fefc3ff1bc41eaec7cd4e1be7e1146a7f6dde
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2024-12-15 20:41:35 +0100

    UnitTest per la funció GestioError fets. Fixes #7

--- UnitTest_acfour_sergio_vegas/Test1.cs
+++ UnitTest_acfour_sergio_vegas/Test1.cs
@@ -1,11 +1,33 @@
-﻿namespace UnitTest_acfour_sergio_vegas
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Runtime.InteropServices;
+
+namespace UnitTest_acfour_sergio_vegas
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class UnitTest_acfour_sergio_vegas
     {
         [TestMethod]
-        public void TestMethod1()
+
+        //Arrange
+        [DataRow(5,5)]
+        public void GestioErrorCorrect(int userNumb, int expected)
         {
+            //Act
+            acfour_sergio_vegas.implementacioDiagrama.GestioError(userNumb);
+            //Assert
+            Assert.AreEqual(expected, userNumb);
+        }   
+    
+        [TestMethod]
+
+        //Arrange
+        [DataRow("sds")]
+        public void GestioErrorIncorrect(int userNumb)
+        {
+            //Act
+            acfour_sergio_vegas.implementacioDiagrama.GestioError(userNumb);
+            //Assert
+            Assert.ThrowsException(FormatException); //No he trobat cap informació per poder fer aquest unittest.
         }
     }
 }
