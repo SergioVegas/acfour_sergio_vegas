a223c42b925aca16d60744baafe1aafa125d3647
Sergio Vegas <sergio.vegas.7e8@itb.cat>
2024-12-15 13:27:37 +0100

    UnitTest creat

---
+++ UnitTest_acfour_sergio_vegas/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitTest_acfour_sergio_vegas
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
